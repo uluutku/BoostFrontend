655f0113b5aeb32ef0cc599865192f36e44aa856
Utku Ulu <utku.ulu@bilgeadamboost.com>
2023-11-30 14:55:28 +0300

    functions solved

--- 01-JavaScript/06-loops.js
+++ 01-JavaScript/06-loops.js
@@ -1,34 +1,34 @@
-// -------------------For - Başlangıç Bitiş Arası Sayıların Toplamı-----------------------
-// kullanıcıdan bir başlangıç sayısı bir de bitiş sayısı isteyin. Bir for döngüsü yaratarak başlangıç sayısından bitiş sayısına kadar bütün sayıları toplayın. toplamı ekrana yazdırın (başlangıç ve bitiş sayısı dahil)
+// // -------------------For - Başlangıç Bitiş Arası Sayıların Toplamı-----------------------
+// // kullanıcıdan bir başlangıç sayısı bir de bitiş sayısı isteyin. Bir for döngüsü yaratarak başlangıç sayısından bitiş sayısına kadar bütün sayıları toplayın. toplamı ekrana yazdırın (başlangıç ve bitiş sayısı dahil)
 
-let baslangic = Number(prompt("Başlangıç sayısını giriniz: "));
-let bitis = Number(prompt("Bitiş sayısını giriniz: "));
-let toplam = 0;
+// let baslangic = Number(prompt("Başlangıç sayısını giriniz: "));
+// let bitis = Number(prompt("Bitiş sayısını giriniz: "));
+// let toplam = 0;
 
-for (let i = baslangic; i <= bitis; i++) {
-    toplam += i;
-    }
-console.log(toplam);
+// for (let i = baslangic; i <= bitis; i++) {
+//     toplam += i;
+//     }
+// console.log(toplam);
 
-// -------------------For - Dersi Geçen Öğrenci Sayısı-----------------------
-// Bir sınıfta 5 öğrenci olduğunu düşünelim. Öğrencilerin notlarını almak için prompt kullanarak her öğrenci için bir not girişi yapın. Ardından, bu notları kullanarak sınıf ortalamasını hesaplayın. Son olarak, sınıftaki öğrencilerin kaç tanesinin geçtiğini (geçer not 60) bulun ve konsola yazdırın.
+// // -------------------For - Dersi Geçen Öğrenci Sayısı-----------------------
+// // Bir sınıfta 5 öğrenci olduğunu düşünelim. Öğrencilerin notlarını almak için prompt kullanarak her öğrenci için bir not girişi yapın. Ardından, bu notları kullanarak sınıf ortalamasını hesaplayın. Son olarak, sınıftaki öğrencilerin kaç tanesinin geçtiğini (geçer not 60) bulun ve konsola yazdırın.
 
-let ogrenciSayisi = 5;
-let notlar = [];
-let toplamNot = 0;
-let gecenOgrenciSayisi = 0;
+// let ogrenciSayisi = 5;
+// let notlar = [];
+// let toplamNot = 0;
+// let gecenOgrenciSayisi = 0;
 
-for (let i = 0; i < ogrenciSayisi; i++) {
-    notlar.push(Number(prompt(`${i+1}. öğrencinin notunu giriniz: `)));
-    toplamNot += notlar[i];
-    if (notlar[i] >= 60) {
-        gecenOgrenciSayisi++;
-    }
-}
+// for (let i = 0; i < ogrenciSayisi; i++) {
+//     notlar.push(Number(prompt(`${i+1}. öğrencinin notunu giriniz: `)));
+//     toplamNot += notlar[i];
+//     if (notlar[i] >= 60) {
+//         gecenOgrenciSayisi++;
+//     }
+// }
 
-console.log(`Sınıf ortalaması: ${toplamNot / ogrenciSayisi}`);
-console.log(`${gecenOgrenciSayisi} öğrenci geçti.`);
-console.log(notlar);
+// console.log(`Sınıf ortalaması: ${toplamNot / ogrenciSayisi}`);
+// console.log(`${gecenOgrenciSayisi} öğrenci geçti.`);
+// console.log(notlar);
 
 // -------------------While - Sayı Tahmin uygulaması-----------------------
 // Random bir sayı tanımlayın ve kullanıcıdan 1-10 arası bir sayı girerek sayıyı tahmin etmesini isteyin. Toplam 3 hak tanımlayın. While döngüsü kullanarak, kullanıcının hakları bitene kadar denemesine izin verin. Her denemenin sonunda sayıyı arttırıp azaltması gerektiği bilgisini ekrana yazdırın.
@@ -38,40 +38,43 @@ let tahminHakki = 3;
 let tahmin;
 
 while (tahminHakki > 0) {
+    tahminHakki--;
     tahmin = Number(prompt("1-10 arası bir sayı giriniz: "));
     if (tahmin === randomSayi) {
         console.log("Tebrikler, doğru tahmin!");
         break;
     } else if (tahmin < randomSayi) {
-        console.log("Daha büyük bir sayı giriniz.");
+        console.log("Daha büyük bir sayı giriniz." + "Kalan hakkınız:" + tahminHakki);
+       
     } else {
         console.log("Daha küçük bir sayı giriniz.");
+        console.log("Daha büyük bir sayı giriniz." + "Kalan hakkınız:" + tahminHakki);
     }
-    tahminHakki--;
+    
 }
 
 if (tahminHakki === 0) {
     console.log("Tahmin hakkınız kalmadı oyun bitti.");
 }   
 
-// -------------------While - Alışveriş Sepeti Uygulaması-----------------------
-// Kurallar:
-// While döngüsü kullanılacak.
-// Kullanıcı prompta herhangi bir ürün ismi veya "onayla" yazabilir.
-// Ürün ismi yazarsa kullanıcıdan ürünün fiyat bilgisini isteyin ve toplam fiyata ekleyin.
-// Kullanıcı "onayla" yazarsa, ekrana sepetin toplam tutar bilgisini yazdırın.
+// // -------------------While - Alışveriş Sepeti Uygulaması-----------------------
+// // Kurallar:
+// // While döngüsü kullanılacak.
+// // Kullanıcı prompta herhangi bir ürün ismi veya "onayla" yazabilir.
+// // Ürün ismi yazarsa kullanıcıdan ürünün fiyat bilgisini isteyin ve toplam fiyata ekleyin.
+// // Kullanıcı "onayla" yazarsa, ekrana sepetin toplam tutar bilgisini yazdırın.
 
-let toplamFiyat = 0;
-let urunAdi;
-let urunFiyati;
+// let toplamFiyat = 0;
+// let urunAdi;
+// let urunFiyati;
 
-while (urunAdi !== "onayla") {
-    urunAdi = prompt("Ürün adı giriniz: ");
-    if (urunAdi === "onayla") {
-        console.log(`Sepet tutarı: ${toplamFiyat}`);
-        break;
-    }
-    urunFiyati = Number(prompt("Ürün fiyatı giriniz: "));
-    toplamFiyat += urunFiyati;
-}
+// while (urunAdi !== "onayla") {
+//     urunAdi = prompt("Ürün adı giriniz: ");
+//     if (urunAdi === "onayla") {
+//         console.log(`Sepet tutarı: ${toplamFiyat}`);
+//         break;
+//     }
+//     urunFiyati = Number(prompt("Ürün fiyatı giriniz: "));
+//     toplamFiyat += urunFiyati;
+// }
 
---
+++ 01-JavaScript/07-functions.js
@@ -0,0 +1,143 @@
+// Yazacağınız fonksiyonlar içerisinde console.log ifadesi yer almayacak.
+// 1- Verilen iki sayının çarpımını döndüren bir fonksiyon yazın. (Arrow Function ile)
+
+const carpim = (sayi1, sayi2) => sayi1 * sayi2;
+console.log(carpim(5, 6));
+
+// 2- Verilen bir cümlenin kelime sayısını döndüren bir arrow fonksiyon yazın.
+
+const kelimeSayisi = (cumle) => cumle.split(" ").length;
+console.log(kelimeSayisi("Merhaba benim adım Ayşe"));
+
+// 3- Verilen bir sayının faktöriyelini hesaplayan bir fonksiyon yazın. (Function Declaration Yöntemi ile)
+
+function faktoriyel(sayi) {
+    let sonuc = 1;
+    for (let i = 1; i <= sayi; i++) {
+        sonuc *= i;
+    }
+    return sonuc;
+}
+console.log(faktoriyel(5));
+
+// 4- Verilen bir sayının asal olup olmadığını kontrol eden bir fonksiyon yazın. (Function Expression Yöntemi ile)
+
+const asalMi = function (sayi) {
+    if (sayi === 1) {
+        return false;
+    } else if (sayi === 2) {
+        return true;
+    } else {
+        for (let i = 2; i < sayi; i++) {
+            if (sayi % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+console.log(asalMi(7));
+
+// 5- Verilen bir dizi içindeki en büyük sayıyı bulan bir fonksiyon yazın.
+
+const enBuyuk = function (dizi) {
+    let enBuyuk = dizi[0];
+    for (let i = 1; i < dizi.length; i++) {
+        if (dizi[i] > enBuyuk) {
+            enBuyuk = dizi[i];
+        }
+    }
+    return enBuyuk;
+}
+
+
+
+// 6- Verilen bir dizi içindeki sayıların toplamını hesaplayan bir fonksiyon yazın.
+
+const toplam = function (dizi) {
+    let toplam = 0;
+    for (let i = 0; i < dizi.length; i++) {
+        toplam += dizi[i];
+    }
+    return toplam;
+}
+
+
+// 7-Yukarıdaki son iki fonksiyonu arrow function olarak yeniden yazın.
+
+const enBuyuk2 = (dizi) => {
+    let enBuyuk = dizi[0];
+    for (let i = 1; i < dizi.length; i++) {
+        if (dizi[i] > enBuyuk) {
+            enBuyuk = dizi[i];
+        }
+    }
+    return enBuyuk;
+}
+
+// Buradan itibaren yalnızca arrow function kullanıyoruz.
+
+const toplam2 = (dizi) => {
+    let toplam = 0;
+    for (let i = 0; i < dizi.length; i++) {
+        toplam += dizi[i];
+    }
+    return toplam;
+}
+
+// 8- Verilen bir stringi tersine çeviren bir fonksiyon yazın.
+
+const tersCevir = (string) => {
+    let tersString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        tersString += string[i];
+    }
+    return tersString;
+}
+
+// 9- Verilen bir dizinin elemanlarını rastgele karıştıran bir fonksiyon yazın.
+
+const karistir = (dizi) => {
+    let karisikDizi = [];
+    while (dizi.length > 0) {
+        let randomIndex = Math.floor(Math.random() * dizi.length);
+        karisikDizi.push(dizi[randomIndex]);
+        dizi.splice(randomIndex, 1);
+    }
+    return karisikDizi;
+}
+
+// 10- Verilen bir sayının pozitif bölenlerini bulan bir fonksiyon yazın.(örn. 12'nin pozitif bölenleri 1,2,3,4,6,12)
+
+const pozitifBolenler = (sayi) => {
+    let bolenler = [];
+    for (let i = 1; i <= sayi; i++) {
+        if (sayi % i === 0) {
+            bolenler.push(i);
+        }
+    }
+    return bolenler;
+}
+
+// 11- Verilen bir stringin içinde belirli bir karakterin kaç kez tekrarlandığını hesaplayan bir fonksiyon yazın.
+
+const karakterSayisi = (string, karakter) => {
+    let sayac = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === karakter) {
+            sayac++;
+        }
+    }
+    return sayac;
+}
+
+// 12 - içerisine kaç adet sayı yazılırsa yazılsın, sayıların toplamını döndüren bir fonksiyon yazınız.
+
+const toplam3 = (...sayilar) => {
+    let toplam = 0;
+    for (let i = 0; i < sayilar.length; i++) {
+        toplam += sayilar[i];
+    }
+    return toplam;
+}
